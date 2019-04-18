113b3ba288521507274a028eb6a952eb8c06d26e
Alixfrereau <47738461+Alixfrereau@users.noreply.github.com>
2019-04-18 19:02:05 +0200

    section-> div + modif

--- spaceinvaderJS/scripts/main.js
+++ spaceinvaderJS/scripts/main.js
@@ -1,24 +1,24 @@
-let div1= document.querySelector(".div1")
-let div2= document.querySelector(".div2")
-let annonceButton= document.querySelector(".annonceButton")
-let close= document.querySelector(".close")
+let div1 = document.querySelector(".div1")
+let div2 = document.querySelector(".div2")
+let annonceButton = document.querySelector(".annonceButton")
+let close = document.querySelector(".close")
 
 annonceButton.addEventListener(
   'click',
   function() {
-    div1.style.transform= "translateY(-245px)"
-    div1.style.transition="all 3s"
-    div2.style.transform= "translateY(225px)"
-    div2.style.transition= "all 3s"
+    div1.style.transform = "translateY(-245px)"
+    div1.style.transition ="all 3s"
+    div2.style.transform = "translateY(225px)"
+    div2.style.transition = "all 3s"
   }
 )
 
 close.addEventListener(
   'click',
   function() {
-    div1.style.transform= "translateY(0px)"
-    div1.style.transition="all 3s"
-    div2.style.transform= "translateY(0px)"
-    div2.style.transition= "all 3s"
+    div1.style.transform = "translateY(0px)"
+    div1.style.transition ="all 3s"
+    div2.style.transform = "translateY(0px)"
+    div2.style.transition = "all 3s"
   }
 )
